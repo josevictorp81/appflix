1ce665992cdc0a650fed1e298a7a2cb1c2aee3f2
José Victor Pereira Lima <josevictorp81@gmail.com>
2022-02-19 16:31:27 -0300

    addicionando pagina home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:appflix/pages/home.dart';
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -22,8 +23,17 @@ class _HomePageState extends State<HomePage> {
     // final _pageOptions = [];
     return Scaffold(
       backgroundColor: const Color(0xff111820),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff111820),
+        title: Text('App'),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Text('Body'),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Home(),
+          ),
+        ),
       ),
       bottomNavigationBar: DotNavigationBar(
         margin: EdgeInsets.only(left: 10, right: 10),
