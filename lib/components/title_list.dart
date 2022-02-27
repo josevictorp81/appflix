461764fab8d339c4070ea0cc4fd5401c7f3dc8dc
José Victor Pereira Lima <josevictorp81@gmail.com>
2022-02-27 20:53:25 -0300

    componente title list

---
+++ lib/components/title_list.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TitleList extends StatelessWidget {
+  const TitleList(
+      {Key? key, required this.width, required this.text, this.top = 5})
+      : super(key: key);
+
+  final double width;
+  final String text;
+  final double top;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: width * .1, bottom: 10, top: top),
+      child: Row(
+        children: [
+          Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          )
+        ],
+      ),
+    );
+  }
+}
