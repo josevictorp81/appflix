006fbf253626d33a5b479c7d3c868dba01349bce
José Victor Pereira Lima <josevictorp81@gmail.com>
2022-02-28 16:52:46 -0300

    adicionando sizedbox

--- lib/components/card_movie.dart
+++ lib/components/card_movie.dart
@@ -9,25 +9,30 @@ class CardMovie extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 140,
-          height: 160,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            image: DecorationImage(
-              image: NetworkImage('https://image.tmdb.org/t/p/w780/$urlImage'),
-              fit: BoxFit.cover,
+    return SizedBox(
+      width: 150,
+      child: Column(
+        children: [
+          Container(
+            width: 140,
+            height: 160,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                image:
+                    NetworkImage('https://image.tmdb.org/t/p/w780/$urlImage'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Text(
-          name,
-          style: TextStyle(color: Colors.white, fontSize: 12),
-        ),
-      ],
+          Text(
+            name,
+            style: TextStyle(color: Colors.white, fontSize: 12),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
