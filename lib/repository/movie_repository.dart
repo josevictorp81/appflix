7df1a38c779a87985fc1f1976bff5e5deda33170
José Victor Pereira Lima <josevictorp81@gmail.com>
2022-03-01 09:45:19 -0300

    função de buscar filmes

--- lib/repository/movie_repository.dart
+++ lib/repository/movie_repository.dart
@@ -26,4 +26,24 @@ class MovieRepository {
       throw Exception(e);
     }
   }
+
+  Future<List<Movie>> searchMovie(
+      int page, String endpoint, String movie) async {
+    var params = {
+      'language': 'pt-br',
+      'api_key': 'de0279ac33da1f3ef47fa098401b0a99',
+      'page': page,
+      'query': movie,
+    };
+    try {
+      final Response response =
+          await _client.get(endpoint, queryParameters: params);
+      if (response.statusCode == 200) {
+        return Movie.jsonToList(response.data['results']);
+      }
+      return [];
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
 }
