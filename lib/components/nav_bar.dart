e6af4769d094b1b835a2cba9af578ee37cedce6f
José Victor Pereira Lima <josevictorp81@gmail.com>
2022-03-01 14:41:05 -0300

    componente top navbar

---
+++ lib/components/nav_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key, required this.pathName, required this.text})
+      : super(key: key);
+
+  final String pathName;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(pathName);
+      },
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 20, color: Colors.white),
+      ),
+    );
+  }
+}
